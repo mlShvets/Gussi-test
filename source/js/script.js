e75d43fac6ec553e3134546a1a7360da5f707731
Dmitrii Shvetsov <Shvec335@gmail.com>
2021-10-11 01:08:28 +0300

    Счётчик слайдов

--- source/js/script.js
+++ source/js/script.js
@@ -1,10 +1,17 @@
-// JS
 const accordions = document.querySelectorAll('.services__accordion');
 const minusBtn = document.querySelector('.quantity__btn--minus');
 const plusBtn = document.querySelector('.quantity__btn--plus');
 const quantityNumber = document.querySelector('.quantity__number');
 const cost = document.querySelector('.cost__number');
+const slides = document.querySelectorAll('.slider__item');
+const sliderNumber = document.querySelector('.slider__number');
+const sliderTotal = document.querySelector('.slider__total');
+const prevBtn = document.querySelector('.slider__btn--prev');
+const nextBtn = document.querySelector('.slider__btn--next');
+let index = 0;
 const PRICE = 120;
+
+//accordion
 
 for (const accordion of accordions) {
   accordion.addEventListener('click', function () {
@@ -20,17 +27,16 @@ for (const accordion of accordions) {
 
 //slider
 
-const slides = document.querySelectorAll('.slider__item');
-const sliderTotal = document.querySelector('.slider__total');
-const prevBtn = document.querySelector('.slider__btn--prev');
-const nextBtn = document.querySelector('.slider__btn--next');
-let index = 0;
-
 const activeSlide = (number) => {
   for (const slide of slides) {
     slide.classList.remove('slider__item--current');
   }
   slides[number].classList.add('slider__item--current');
+  slides.forEach((slide) => {
+    if (slide.classList.contains('slider__item--current')) {
+      sliderNumber.textContent = `0${number+1}`;
+    }
+  });
 };
 
 const nextSlide = () => {
@@ -55,15 +61,6 @@ const prevSlide = () => {
 
 nextBtn.addEventListener('click', nextSlide);
 prevBtn.addEventListener('click', prevSlide);
-
-//slider counter
-
-// const sliderNumber = document.querySelector('.slider__number');
-// slides.forEach((slide, i) => {
-//   if (slide.classList.contains('slider__item--current')) {
-//     sliderNumber.textContent = `0${i+1}`;
-//   }
-// });
 
 slides.length < 10 ? sliderTotal.textContent = `0${slides.length}` : sliderTotal.textContent = `${slides.length}`;
 
