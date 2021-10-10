7c71a9c4e186fc17573e0112b4c09c726ae5f547
Dmitrii Shvetsov <Shvec335@gmail.com>
2021-10-10 05:54:47 +0300

    Реализация счётчика затрат и переключеня слайдов

--- source/js/script.js
+++ source/js/script.js
@@ -3,6 +3,8 @@ const accordions = document.querySelectorAll('.services__accordion');
 const minusBtn = document.querySelector('.quantity__btn--minus');
 const plusBtn = document.querySelector('.quantity__btn--plus');
 const quantityNumber = document.querySelector('.quantity__number');
+const cost = document.querySelector('.cost__number');
+const PRICE = 120;
 
 for (const accordion of accordions) {
   accordion.addEventListener('click', function () {
@@ -16,11 +18,63 @@ for (const accordion of accordions) {
   });
 }
 
+//slider
+
+const slides = document.querySelectorAll('.slider__item');
+const sliderTotal = document.querySelector('.slider__total');
+const prevBtn = document.querySelector('.slider__btn--prev');
+const nextBtn = document.querySelector('.slider__btn--next');
+let index = 0;
+
+const activeSlide = (number) => {
+  for (const slide of slides) {
+    slide.classList.remove('slider__item--current');
+  }
+  slides[number].classList.add('slider__item--current');
+};
+
+const nextSlide = () => {
+  if (index === slides.length - 1) {
+    index = 0;
+    activeSlide(index);
+  } else {
+    index++;
+    activeSlide(index);
+  }
+};
+
+const prevSlide = () => {
+  if (index === 0) {
+    index = slides.length - 1;
+    activeSlide(index);
+  } else {
+    index--;
+    activeSlide(index);
+  }
+};
+
+nextBtn.addEventListener('click', nextSlide);
+prevBtn.addEventListener('click', prevSlide);
+
+//slider counter
+
+// const sliderNumber = document.querySelector('.slider__number');
+// slides.forEach((slide, i) => {
+//   if (slide.classList.contains('slider__item--current')) {
+//     sliderNumber.textContent = `0${i+1}`;
+//   }
+// });
+
+slides.length < 10 ? sliderTotal.textContent = `0${slides.length}` : sliderTotal.textContent = `${slides.length}`;
+
 //quantity counter
+
+const getCosts = (count) => `${count*PRICE}`;
 
 const getQuantity = () => {
   plusBtn.addEventListener('click', () => {
     quantityNumber.value++;
+    cost.textContent = getCosts(quantityNumber.value);
     if (quantityNumber.value > '1') {
       minusBtn.removeAttribute('disabled');
     }
@@ -29,6 +83,7 @@ const getQuantity = () => {
     if (quantityNumber.value > '1') {
       minusBtn.removeAttribute('disabled');
       quantityNumber.value--;
+      cost.textContent = getCosts(quantityNumber.value);
     } else {
       minusBtn.setAttribute('disabled', 'disabled');
     }
